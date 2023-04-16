725a3505eeb4e942c5165be939b700e52286fe4b
nlog <nlog675@gmail.com>
2023-04-16 15:32:14 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import listSvg from './assets/img/list.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__sidebar">
+        <ul className="todo__list">
+          <li>
+            <i>
+              <img src={listSvg} alt="иконка списка"/>
+            </i>
+            <span>Все задачи</span>
+          </li>
+        </ul>
+      </div>
+      <div className="todo__tasks"></div>
     </div>
   );
 }
