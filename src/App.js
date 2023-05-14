88794f16fd098d538448f9dd5968f0229efb932a
nlog <nlog675@gmail.com>
2023-05-14 16:30:08 +0300

    add markup of the left side with the list of tasks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React from "react";
-import List from "./components/List";
-import listSvg from './assets/img/list.svg'
+import React from 'react';
+import List from './components/List';
+import listSvg from './assets/img/list.svg';
+import plusSvg from './assets/img/add.svg';
 
 function App() {
   return (
     <div className="todo">
       <div className="todo__sidebar">
-        <List items={[
-          {
-            icon: listSvg,
-            name: 'Все задачи',
-            active: true,
-          }
-        ]} />
-        <List items={[
-          {
-            color: 'green',
-            name: 'Покупки'
-          },
-          {
-            color: 'blue',
-            name: 'Фронтенд'
-          },
-          {
-            color: 'pink',
-            name: 'Фильмы и сериалы'
-          },
-        ]}/>
+        <List
+          items={[
+            {
+              
+              icon: listSvg,
+              name: 'Все задачи',
+              active: true,
+            },
+          ]}
+        />
+        <List
+          items={[
+            {
+              color: 'green',
+              name: 'Покупки',
+            },
+            {
+              color: 'blue',
+              name: 'Фронтенд',
+            },
+            {
+              color: 'pink',
+              name: 'Фильмы и сериалы',
+            },
+          ]}
+          isRemovable
+        />
+        <List
+          items={[
+            {
+              className: 'list__add-button',
+              icon: plusSvg,
+              name: 'Добавить список',
+            },
+          ]}
+        />
       </div>
       <div className="todo__tasks"></div>
     </div>
